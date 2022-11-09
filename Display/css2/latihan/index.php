b4aef55502efe42483fcadd83c59ea35e16be63b
MuhamadZidanTriyadi <muhammadzidantriyadi@gmamil.com>
2022-11-09 16:27:27 +0700

    CSS4-DISPLAY

---
+++ Display/css2/latihan/index.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Latihan Merubah Display</title>
+        <link rel="stylesheet" href="style1.css">
+    </head>
+    <body>
+
+        <div class="wrap">
+            <div class="header">
+                <h1>Hello Saya bisa CSS</h1>
+            </div>
+
+        <div class="navbar">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Profile</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">Konten</a></li>
+            </ul> 
+        </div>
+
+        <section>
+            <img class="tengah" src="astronot.jpg" />
+        </section>
+
+        <div class="konten">
+            <h2>Css itu mudah sekali</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem at rerum quia aperiam doloribus 
+            blanditiis ipsam tenetur eveniet sunt. Illo, cupiditate? Itaque reprehenderit nihil atque harum aperiam 
+            architecto quisquam mollitia?</p>
+        </div>
+
+    </div>
+
+    </body>
+</html>
